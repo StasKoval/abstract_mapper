2d78c192378c63ca37f22fd274eb77eb0dec098b
Andrew Kozin <andrew.kozin@gmail.com>
2015-07-04 03:10:02 +0300

    [FEATURE] Add shared examples for testing mapper settings
    The examples can be used to specify domain-specific mappers:
    
      it_behaves_like :defining_command do
        let(:name) { :list }
        let(:node) { List  }
      end
    
      it_behaves_like :defining_rule do
        let(:rule) { CompactLists }
      end
    
    == Added
    
    * `:defining_command` shared example
    * `:defining_rule` shared example

--- lib/abstract_mapper/rspec.rb
+++ lib/abstract_mapper/rspec.rb
@@ -4,3 +4,4 @@
 require_relative "../rspec/functions"
 require_relative "../rspec/nodes"
 require_relative "../rspec/rules"
+require_relative "../rspec/mapper"
---
+++ lib/rspec/mapper.rb
@@ -0,0 +1,40 @@
+# encoding: utf-8
+
+# ==============================================================================
+# Examples for testing DSL registry
+# ==============================================================================
+
+shared_examples :defining_rule do
+
+  subject { described_class.settings.rules.registry }
+  it do
+    is_expected.to include(rule), <<-REPORT.gsub(/.+\|/, "")
+      |
+      |#{described_class} optimization rules
+      |
+      |expected to include #{rule.inspect}
+      |     got rules:
+      |#{subject.map { |rule| "#{" " * 9}- #{rule.inspect}" }.join("\n")}
+    REPORT
+  end
+
+end # shared examples
+
+shared_examples :defining_command do
+
+  subject { described_class.settings.commands.registry }
+  it "registers the command" do
+    expect(subject[name]).to eql(node), <<-REPORT.gsub(/.+\|/, "")
+      |
+      |#{described_class} DSL commands
+      |
+      |expected to include '#{name}' adding #{node.inspect}
+      |     got commands:
+      |#{subject
+        .map { |name, node| "#{" " * 9}- #{name}: #{node.inspect}" }
+        .sort
+        .join("\n")}
+    REPORT
+  end
+
+end # shared examples
